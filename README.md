d15bf31897701811fa8b756d0df8c60cc3e92512
Robin Miller <robin.marie.miller@outlook.com>
2015-12-27 12:52:35 -0800

    added code book and completed first 3 steps

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # Tidy Data Project
 ## Files submitted in the repository
-*README.md*: This file - provides a list of files submitted and a description of run_analysis.R
-*run_analysis.R*: The R script which takes the input data and produces a tidy data set
-*CodeBook.md*: A description of the variables contained in the final tidy data set
-*UCI HAR dataset*: The folder containing the original data files and descriptions of the original data
+__README.md__: This file - provides a list of files submitted and a description of run_analysis.R
 
-## run_analysis.R
+__run_analysis.R__: The R script which takes the original data and produces a tidy data set
+
+__CodeBook.md__: A description of the variables contained in the final tidy data set
+
+__UCI HAR dataset__: The folder containing the original data files and descriptions provided in the assignment description
+
+__TBD__: The final tidy data set that meets the three tidy data criteria:
+* each variable is a column
+* each observation is a row
+* each type of observational unit is a table
+
+## Running the run_analysis.R script to produce the tidy data set
+run_analysis.R assumes that the "UCI HAR dataset" folder is in the same folder as the script itself. Since the assignment instructions only require use of the actual measurments from the experiments and do not require information about the subjects themselves, I only use the X_test and X_train data files.
+
+run_analysis.R also assumes you have installed the dplyr and tidyr packages. If you have not, please run <install.packages("dplyr")> and/or <install.packages("tidyr")> before running run_analysis.R.
+
+__Step 1:__ The script will first merge the X_test and y_test data so that the activity type is a column alongside the results from the test, and similarly with the X_train and y_train data. Then it will merge the test and train data into a single data set by rows. It will remove all but the final combined data set for tidiness.
+
+__Step 2:__ The variable descriptions for the original data show that some represent mean, standard deviation, min, max, and others. The assignment is only concerned with the values that represent mean and standard deviation so the script pulls out the subset of columns which correspond to either a mean or standard deviation variables as specified in the features.txt file. Again, for tidiness, it then cleans up the unnecessary larger data table.
+
+__Step 3:__ The script turns the activity number into a factor, and then assigns new human-readable labels to the factors. I used the mapping defined in activity_labels.txt
+
+__Step 4:__ 
+
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,49 @@
 library(dplyr)
-
-dataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-
-
+library(tidyr)
 ##  Merges the training and the test sets to create one data set.
-##  Extracts only the measurements on the mean and standard deviation 
-##      for each measurement. 
+testData <- tbl_df(read.table("./UCI HAR Dataset/test/X_test.txt"))
+testType <- tbl_df(read.table("./UCI HAR Dataset/test/y_test.txt"))
+names(testType) <- "activity"
+trainData <- tbl_df(read.table("./UCI HAR Dataset/train/X_train.txt"))
+trainType <- tbl_df(read.table("./UCI HAR Dataset/train/y_train.txt"))
+names(trainType) <- "activity"
+combinedTest <- bind_cols(testType, testData)
+combinedTrain <- bind_cols(trainType, trainData)
+combinedData <- bind_rows(combinedTest, combinedTrain)
+rm("testData"); rm("trainData"); rm("testType"); rm("trainType")
+rm("combinedTest"); rm("combinedTrain")
+##  Extracts only the measurements on the mean and standard deviation for each measurement. 
+meanAndStdDevData <- select(combinedData, activity, V1:V6, V41:V46, V81:V86, V121:V126, V161:V166, 
+                            V201:V202, V214:V215, V227:V228, V240:V241, V253:V254, V266:V271,
+                            V345:V350, V373:V375, V424:V429, V503:V504, V516:V517, V529:V530,
+                            V552, V555:V561)
+rm("combinedData")
 ##  Uses descriptive activity names to name the activities in the data set
+meanAndStdDevData$activity <- as.factor(meanAndStdDevData$activity)
+levels(meanAndStdDevData$activity) <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS",
+                                        "SITTING", "STANDING", "LAYING")
+
+
+
+columnNames <- read.table("./UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+for(i in seq_along(meanAndStdDevData)){
+    vNum <- extract_numeric(names(meanAndStdDevData[i]))
+    names(meanAndStdDevData)[i] = columnNames[[2]][vNum]
+}
+
+
 ##  Appropriately labels the data set with descriptive variable names. 
 ##  From the data set in step 4, creates a second, independent tidy data set 
 ##      with the average of each variable for each activity and each subject.
+
+
+##by the end of Step 3 you have replaced the numeric codes for activity with 
+##  the corresponding alphabetic value from another of the data files;
+##by the end of Step 4 you have modified the original variable names (column names) 
+##  with names conforming to both the R naming standard and the convention
+##  introduced by the tidy data principles; domain experts would likely 
+##  consider the names as already easy to read but you want non-domain experts 
+##  to understand the content of each column; and
+##by the end of Step 5 you have a separate tidy data set containing 
+##  observations consisting of study participants, activities performed during 
+##  the study, and a computed average for each of the measurement variables.
